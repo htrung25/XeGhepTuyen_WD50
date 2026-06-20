806d1a29fa1aded322f3f53506efabd355ed7730
htrung25 <huutrung296sb@gmail.com>
2026-06-20 09:00:00 +0700

    feat(router): add customer portal Vue Router with auth guards

---
+++ resources/js/router/customer.router.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { customerRoutes, setupCustomerGuard } from './customer.routes'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: customerRoutes,
+  scrollBehavior: () => ({ top: 0 }),
+})
+
+setupCustomerGuard(router)
+
+export default router
